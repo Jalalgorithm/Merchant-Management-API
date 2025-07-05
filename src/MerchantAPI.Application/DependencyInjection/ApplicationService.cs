4ebd6a27596428194cba17cdca308777a9c028cd
Abdul-Jalal Ahmeed <temitomzi@gmail.com>
2025-07-05 23:43:55 +0100

    Registered the DI for each library

---
+++ src/MerchantAPI.Application/DependencyInjection/ApplicationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MerchantAPI.Application.DependencyInjection
+{
+    public static class ApplicationService
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // Register application services here
+            return services;
+        }
+    }
+}
---
+++ src/MerchantAPI.Infrastructure/DependencyInjection/InfrastructureService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MerchantAPI.Infrastructure.DependencyInjection
+{
+    public static class InfrastructureService
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            // Register your infrastructure services here
+            // Example: services.AddScoped<IMyService, MyService>();
+            return services;
+        }
+    }
+}
