8f0e941f81acc8394a1e48c78d67470ad279dc8f
Abdul-Jalal Ahmeed <temitomzi@gmail.com>
2025-07-05 23:42:34 +0100

    Added the Merchant entity

---
+++ src/MerchantAPI.Domain/Entities/Merchant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantAPI.Domain.Entities
+{
+    public class Merchant
+    {
+        public int Id { get; set; }
+        public string BusinessName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
