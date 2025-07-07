61bb1e0d0ac8397a99911548db001e441f9aa97e
Abdul-Jalal Ahmeed <temitomzi@gmail.com>
2025-07-07 23:11:24 +0100

    Added DbContext

---
+++ src/MerchantAPI.Application/Commons/Data/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MerchantAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MerchantAPI.Application.Commons.Data
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Merchant> Merchants { get;}
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/MerchantAPI.Infrastructure/Configuration/MerchantConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MerchantAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MerchantAPI.Infrastructure.Configuration
+{
+    internal sealed class MerchantConfiguration : IEntityTypeConfiguration<Merchant>
+    {
+        public void Configure(EntityTypeBuilder<Merchant> builder)
+        {
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.BusinessName)
+                .IsRequired()
+                .HasMaxLength(200);
+            builder.Property(m => m.Country)
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.Property(m => m.CreatedAt)
+                .IsRequired();
+            builder.Property(m=>m.Status).HasConversion<string>().IsRequired();
+            builder.HasIndex(m => m.BusinessName).IsUnique();
+        }
+    }
+}
