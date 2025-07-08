38fd1652b711614b267e6c180d076c0b22ac8b75
Abdul-Jalal Ahmeed <temitomzi@gmail.com>
2025-07-09 00:41:30 +0100

    implemented the country name validator

---
+++ src/MerchantAPI.Application/Commons/Services/ICountryValidatorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantAPI.Infrastructure.Services.CountryValidator
+{
+    public interface ICountryValidatorService
+    {
+        Task<bool> IsValidCountryAsync(string country);
+    }
+}
---
+++ src/MerchantAPI.Infrastructure/Services/CountryValidator/CountryValidatorService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantAPI.Infrastructure.Services.CountryValidator
+{
+    public class CountryValidatorService : ICountryValidatorService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CountryValidatorService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> IsValidCountryAsync(string country)
+        {
+            var response = await _httpClient.GetAsync($"https://restcountries.com/v3.1/name/{Uri.EscapeDataString(country)}?fullText=true");
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
