e5bc1391d5ede76f3e7ac83b50fa2b29c442d50d
Abdul-Jalal Ahmeed <temitomzi@gmail.com>
2025-07-09 00:52:08 +0100

    adjusted the base dto

--- src/MerchantAPI.Application/Features/MerchantBase.cs
+++ src/MerchantAPI.Application/Features/MerchantBase.cs
@@ -12,5 +12,7 @@ namespace MerchantAPI.Application.Features
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Status { get; set; }
+        public string Country { get; set; }
+
     }
 }
