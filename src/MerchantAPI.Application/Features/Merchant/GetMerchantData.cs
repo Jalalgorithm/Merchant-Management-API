ab108faec2c2581243ce88ce9dc8b1715d8718c0
Abdul-Jalal Ahmeed <temitomzi@gmail.com>
2025-07-09 00:49:03 +0100

    get merchant data accessor

---
+++ src/MerchantAPI.Application/Features/Merchant/GetMerchantData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantAPI.Application.Features.Merchant
+{
+    public record GetMerchantData : MerchantBase
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Status { get; set; }
+    }
+}
