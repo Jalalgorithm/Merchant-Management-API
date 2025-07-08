85c332f1b26712d1376d12165366fd8894bc62fa
Abdul-Jalal Ahmeed <temitomzi@gmail.com>
2025-07-09 00:42:18 +0100

    injected all neccessary services

--- src/MerchantAPI.Infrastructure/DependencyInjection/InfrastructureService.cs
+++ src/MerchantAPI.Infrastructure/DependencyInjection/InfrastructureService.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MerchantAPI.Application.Commons.Data;
+using MerchantAPI.Infrastructure.Persistence;
+using MerchantAPI.Infrastructure.Services.CountryValidator;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+
 
 namespace MerchantAPI.Infrastructure.DependencyInjection
 {
@@ -11,9 +16,29 @@ namespace MerchantAPI.Infrastructure.DependencyInjection
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
-            // Register your infrastructure services here
-            // Example: services.AddScoped<IMyService, MyService>();
+            
+            services.AddHttpClient<ICountryValidatorService , CountryValidatorService>();
+            services.AddDatabase();
             return services;
+        }
+
+        private static IServiceCollection AddDatabase(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>options.UseInMemoryDatabase("MerchantAPIDb"));
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+          
+            return services;
+        }
+
+        public static async Task SeedDatabaseAsync(this IServiceProvider services)
+        {
+            using var scope = services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            if (!dbContext.Database.IsInMemory())
+            {
+                await dbContext.Database.EnsureCreatedAsync();
+            }
+            await ApplicationDbSeeder.SeedAsync(dbContext);
         }
     }
 }
