c59e2793e94d846d460846fcaecfa93d519579a4
Abdul-Jalal Ahmeed <temitomzi@gmail.com>
2025-07-09 00:53:13 +0100

    Added all services to the middleware pipeline

--- src/MerchantAPI.WebAPIServer/Program.cs
+++ src/MerchantAPI.WebAPIServer/Program.cs
@@ -1,5 +1,8 @@
 using MerchantAPI.Application.DependencyInjection;
 using MerchantAPI.Infrastructure.DependencyInjection;
+using MerchantAPI.WebAPIServer.DependencyInjection;
+using Serilog;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration));
 builder.Services.AddInfrastructure();
 builder.Services.AddApplication();
+builder.Services.AddWebAPIServer();
 
 var app = builder.Build();
 
@@ -22,8 +28,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseSerilogRequestLogging();
+app.UseExceptionHandler();
 app.UseAuthorization();
+await app.Services.SeedDatabaseAsync();
 
 app.MapControllers();
 
