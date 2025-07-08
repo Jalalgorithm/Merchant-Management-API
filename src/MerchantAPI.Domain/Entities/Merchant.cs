ee653abbdec6c6fbef24214e611b6568a8529d58
Abdul-Jalal Ahmeed <temitomzi@gmail.com>
2025-07-09 00:38:18 +0100

    Added Country to Model

--- src/MerchantAPI.Domain/Entities/Merchant.cs
+++ src/MerchantAPI.Domain/Entities/Merchant.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MerchantAPI.Domain.ValueObjects;
 
 namespace MerchantAPI.Domain.Entities
 {
@@ -12,6 +13,8 @@ namespace MerchantAPI.Domain.Entities
         public string BusinessName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public string Country { get; set; }
+        public Status Status { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
