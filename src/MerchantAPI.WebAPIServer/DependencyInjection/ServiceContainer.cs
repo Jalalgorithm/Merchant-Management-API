7df52613032e456435b487a1e9dc44ec9105e618
Abdul-Jalal Ahmeed <temitomzi@gmail.com>
2025-07-09 00:50:13 +0100

    services injected

---
+++ src/MerchantAPI.WebAPIServer/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,21 @@
+﻿using MerchantAPI.WebAPIServer.Exceptions;
+
+namespace MerchantAPI.WebAPIServer.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddWebAPIServer(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddExceptionHandler<GlobalExceptionHandler>();
+            services.AddProblemDetails();
+            return services;
+        }
+
+        //public static IApplicationBuilder UsePresentation(this IApplicationBuilder app)
+        //{
+        //    app.
+        //    return app;
+        //}
+    }
+}
