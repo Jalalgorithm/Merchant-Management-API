e3ea739591edda34fbcca369cae535ef1ac7a234
Abdul-Jalal Ahmeed <temitomzi@gmail.com>
2025-07-09 00:50:39 +0100

    Added globalexceptionhandler

---
+++ src/MerchantAPI.WebAPIServer/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace MerchantAPI.WebAPIServer.Exceptions
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            Log.Error(exception, "Error occurred");
+            var problemDetails = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Server failure",
+                Type = "https://httpstatuses.org/500",
+            };
+
+            httpContext.Response.StatusCode = problemDetails.Status.Value;
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken: cancellationToken);
+            return true;
+        }
+    }
+}
